4b2c04a94ea19b8fb4553784917e5a693a50de9e
CamiloR66 <56036017+CamiloR66@users.noreply.github.com>
2023-09-12 18:04:43 -0400

    Adding parser

--- TP1_BaseCode/TP1_BaseCode/src/tp1/Devoir1A.java
+++ TP1_BaseCode/TP1_BaseCode/src/tp1/Devoir1A.java
@@ -4,7 +4,17 @@
 
 package tp1;
 
+import java.io.File;
+import java.io.IOException;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 /**
  * Fichier de base pour le Devoir1A du cours IFT287
@@ -26,7 +36,8 @@ package tp1;
  */
 public class Devoir1A
 {
-
+    
+    
     public static void main(String[] args)
     {
         if (args.length < 2)
@@ -39,7 +50,37 @@ public class Devoir1A
         String nomFichierJSON = args[1];
         
         System.out.println("Debut de la conversion du fichier " + nomFichierXML + " vers le fichier " + nomFichierJSON);
-
+       
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        factory.setValidating(true);
+        try
+        {
+            SAXParser parser = factory.newSAXParser();
+            parser.parse(new File(nomFichierXML), new DefaultHandler());     
+            parser.toString();
+            
+            DocumentBuilderFactory builder = DocumentBuilderFactory.newInstance();
+            Document doc = builder.newDocumentBuilder().newDocument();
+            doc.toString();
+        
+        }
+        catch (ParserConfigurationException e)
+        {
+            // TODO Auto-generated catch block
+            
+            e.printStackTrace();
+        }
+        catch (SAXException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            // TODO: handle exception
+        }
+        
+        
         // Votre code de conversion devrait aller ici
         
         
