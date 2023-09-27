e6e38bdf81cf00d2d1021a3a3e255cc3c8c3ed56
Camilo <camilor66@hotmail.com>
2023-09-26 23:20:07 -0400

    Tp2 base

---
+++ AubergeInn/src/AubergeInn/AubergeInn.java
@@ -0,0 +1,207 @@
+// Travail fait par :
+//   NomEquipier1 - Matricule
+//   NomEquipier2 - Matricule
+
+package AubergeInn;
+
+import java.io.*;
+import java.util.StringTokenizer;
+import java.sql.*;
+
+/**
+ * Fichier de base pour le TP2 du cours IFT287
+ *
+ * <pre>
+ * 
+ * Kerson Boisrond
+ * Universite de Sherbrooke
+ * Version 2.0 - 18 Septembre 2023
+ * IFT287 - Exploitation de BD relationnelles et OO
+ * 
+ * Ce programme permet d'appeler des transactions d'un systeme
+ * de gestion utilisant une base de donnees.
+ *
+ * Paramètres du programme
+ * 0- site du serveur SQL ("local" ou "dinf")
+ * 1- nom de la BD
+ * 2- user id pour etablir une connexion avec le serveur SQL
+ * 3- mot de passe pour le user id
+ * 4- fichier de transaction [optionnel]
+ *           si non spécifié, les transactions sont lues au
+ *           clavier (System.in)
+ *
+ * Pré-condition
+ *   - La base de donnees doit exister
+ *
+ * Post-condition
+ *   - Le programme effectue les mises à jour associees à chaque
+ *     transaction
+ * </pre>
+ */
+public class AubergeInn
+{
+    private static Connexion cx;
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception
+    {
+        if (args.length < 4)
+        {
+            System.out.println("Usage: java AubergeInn.AubergeInn <serveur> <bd> <user> <password> [<fichier-transactions>]");
+            return;
+        }
+        
+        cx = null;
+        
+        try
+        {
+            // Il est possible que vous ayez à déplacer la connexion ailleurs.
+            // N'hésitez pas à le faire!
+            cx = new Connexion(args[0], args[1], args[2], args[3]);
+            BufferedReader reader = ouvrirFichier(args);
+            String transaction = lireTransaction(reader);
+            while (!finTransaction(transaction))
+            {
+                executerTransaction(transaction);
+                transaction = lireTransaction(reader);
+            }
+        }
+        finally
+        {
+            if (cx != null)
+                cx.fermer();
+        }
+    }
+
+    /**
+     * Decodage et traitement d'une transaction
+     */
+    static void executerTransaction(String transaction) throws Exception, IFT287Exception
+    {
+        try
+        {
+            System.out.print(transaction);
+            // Decoupage de la transaction en mots
+            StringTokenizer tokenizer = new StringTokenizer(transaction, " ");
+            if (tokenizer.hasMoreTokens())
+            {
+                String command = tokenizer.nextToken();
+                // Vous devez remplacer la chaine "commande1" et "commande2" par
+                // les commandes de votre programme. Vous pouvez ajouter autant
+                // de else if que necessaire. Vous n'avez pas a traiter la
+                // commande "quitter".
+                if (command.equals("commande1"))
+                {
+                    // Lecture des parametres
+                    String param1 = readString(tokenizer);
+                    Date param2 = readDate(tokenizer);
+                    int param3 = readInt(tokenizer);
+                    // Appel de la methode des gestionnaires qui traite la transaction specifique
+                }
+                else if (command.equals("commande2"))
+                {
+                    // Lire les parametres ici et appeler la bonne methode
+                    // de traitement pour la transaction
+                }
+                else
+                {
+                    System.out.println(" : Transaction non reconnue");
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(" " + e.toString());
+            // Ce rollback est ici seulement pour vous aider et éviter des problèmes lors de la correction
+            // automatique. En théorie, il ne sert à rien et ne devrait pas apparaître ici dans un programme
+            // fini et fonctionnel sans bogues.
+            cx.rollback();
+        }
+    }
+
+    
+    // ****************************************************************
+    // *   Les methodes suivantes n'ont pas besoin d'etre modifiees   *
+    // ****************************************************************
+
+    /**
+     * Ouvre le fichier de transaction, ou lit à partir de System.in
+     */
+    public static BufferedReader ouvrirFichier(String[] args) throws FileNotFoundException
+    {
+        if (args.length < 5)
+            // Lecture au clavier
+            return new BufferedReader(new InputStreamReader(System.in));
+        else
+            // Lecture dans le fichier passe en parametre
+            return new BufferedReader(new InputStreamReader(new FileInputStream(args[4])));
+    }
+
+    /**
+     * Lecture d'une transaction
+     */
+    static String lireTransaction(BufferedReader reader) throws IOException
+    {
+        return reader.readLine();
+    }
+
+    /**
+     * Verifie si la fin du traitement des transactions est atteinte.
+     */
+    static boolean finTransaction(String transaction)
+    {
+        // fin de fichier atteinte
+        return (transaction == null || transaction.equals("quitter"));
+    }
+
+    /** Lecture d'une chaine de caracteres de la transaction entree a l'ecran */
+    static String readString(StringTokenizer tokenizer) throws Exception
+    {
+        if (tokenizer.hasMoreElements())
+            return tokenizer.nextToken();
+        else
+            throw new Exception("Autre parametre attendu");
+    }
+
+    /**
+     * Lecture d'un int java de la transaction entree a l'ecran
+     */
+    static int readInt(StringTokenizer tokenizer) throws Exception
+    {
+        if (tokenizer.hasMoreElements())
+        {
+            String token = tokenizer.nextToken();
+            try
+            {
+                return Integer.valueOf(token).intValue();
+            }
+            catch (NumberFormatException e)
+            {
+                throw new Exception("Nombre attendu a la place de \"" + token + "\"");
+            }
+        }
+        else
+            throw new Exception("Autre parametre attendu");
+    }
+
+    static Date readDate(StringTokenizer tokenizer) throws Exception
+    {
+        if (tokenizer.hasMoreElements())
+        {
+            String token = tokenizer.nextToken();
+            try
+            {
+                return Date.valueOf(token);
+            }
+            catch (IllegalArgumentException e)
+            {
+                throw new Exception("Date dans un format invalide - \"" + token + "\"");
+            }
+        }
+        else
+            throw new Exception("Autre parametre attendu");
+    }
+
+}
---
+++ AubergeInn/src/AubergeInn/Connexion.java
@@ -0,0 +1,147 @@
+package AubergeInn;
+
+import java.sql.*;
+
+/**
+ * Gestionnaire d'une connexion avec une BD relationnelle via JDBC.<br><br>
+ * 
+ * Cette classe ouvre une connexion avec une BD via JDBC.<br>
+ * La méthode serveursSupportes() indique les serveurs supportés.<br>
+ * <pre>
+ * Pré-condition
+ *   Le driver JDBC approprié doit être accessible.
+ * 
+ * Post-condition
+ *   La connexion est ouverte en mode autocommit false et sérialisable, 
+ *   (s'il est supporté par le serveur).
+ * </pre>
+ * <br>
+ * IFT287 - Exploitation de BD relationnelles et OO
+ * 
+ * @author Vincent Ducharme - Université de Sherbrooke
+ * @version Version 3.0 - 21 mai 2016
+ *
+ * @author Kerson Boisrond - Université de Sherbrooke
+ * @version Version 3.1 - 17 Septembre 2023
+ *
+ */
+public class Connexion
+{
+    private Connection conn;
+
+    /**
+     * Ouverture d'une connexion en mode autocommit false et sérialisable (si
+     * supporté)
+     * 
+     * @param serveur Le type de serveur SQL à utiliser (Valeur : local, dinf).
+     * @param bd      Le nom de la base de données sur le serveur.
+     * @param user    Le nom d'utilisateur à utiliser pour se connecter à la base de données.
+     * @param pass    Le mot de passe associé à l'utilisateur.
+     */
+    public Connexion(String serveur, String bd, String user, String pass)
+            throws IFT287Exception, SQLException
+    {
+        Driver d;
+        try
+        {
+            d = (Driver)Class.forName("org.postgresql.Driver").newInstance();
+            DriverManager.registerDriver(d);
+            
+            if (serveur.equals("local"))
+            {
+                conn = DriverManager.getConnection("jdbc:postgresql:" + bd, user, pass);
+            }
+            else if (serveur.equals("dinf"))
+            {
+                conn = DriverManager.getConnection("jdbc:postgresql://bd-info2.dinf.usherbrooke.ca:5432/" + bd + "?ssl=true&sslmode=require", user, pass);
+            }
+            else
+            {
+                throw new IFT287Exception("Serveur inconnu");
+            }
+
+            // Mise en mode de commit manuel
+            conn.setAutoCommit(false);
+
+            // Mise en mode sérialisable, si possible
+            // (plus haut niveau d'integrité pour l'accès concurrent aux données)
+            DatabaseMetaData dbmd = conn.getMetaData();
+            if (dbmd.supportsTransactionIsolationLevel(Connection.TRANSACTION_SERIALIZABLE))
+            {
+                conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+                System.out.println("Ouverture de la connexion en mode sérialisable :\n"
+                        + "Connecté sur la BD postgreSQL "
+                        + bd + " avec l'utilisateur " + user);
+            }
+            else
+            {
+                System.out.println("Ouverture de la connexion en mode read committed (default) :\n"
+                        + "Connecté sur la BD postgreSQL "
+                        + bd + " avec l'utilisateur " + user);
+            }
+        }
+        catch (SQLException e)
+        {
+            throw e;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace(System.out);
+            throw new IFT287Exception("JDBC Driver non instancié");
+        }
+    }
+
+    /**
+     * Fermeture d'une connexion
+     */
+    public void fermer() throws SQLException
+    {
+        conn.rollback();
+        conn.close();
+        System.out.println("Connexion fermée " + conn);
+    }
+
+    /**
+     * Commit
+     */
+    public void commit() throws SQLException
+    {
+        conn.commit();
+    }
+
+    public void setIsolationReadCommited() throws SQLException
+    {
+        conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+    }
+
+    /**
+     * Rollback
+     */
+    public void rollback() throws SQLException
+    {
+        conn.rollback();
+    }
+
+    /**
+     * Retourne la Connection JDBC
+     */
+    public Connection getConnection()
+    {
+        return conn;
+    }
+
+    public void setAutoCommit(boolean m) throws SQLException
+    {
+        conn.setAutoCommit(false);
+    }
+
+    /**
+     * Retourne la liste des serveurs supportés par ce gestionnaire de
+     * connexions
+     */
+    public static String serveursSupportes()
+    {
+        return "local : PostgreSQL installé localement\n"
+             + "dinf  : PostgreSQL installé sur les serveurs du département\n";
+    }
+}
